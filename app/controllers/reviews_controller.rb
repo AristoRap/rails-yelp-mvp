f63a5d48c637c7b33c88237402836feffbe4d8dc
Aristotelis Rapai <aristotelisrapai@gmail.com>
2022-01-20 18:12:22 +0100

    added restaurant and review controller actions and views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :find_restaurant, only: %i[new create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(set_review)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_review
+    params.require(:review).permit(:rating, :content)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 end
